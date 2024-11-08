acf21db1747defa3df2feb6a7d5d01dcc79d4564
miklschmidt <miklschmidt@users.noreply.github.com>
2024-11-08 01:59:15 +0000

    Build: (c0bcec0) Templates: fix gcode block indentation in prusa templates

--- build/types/link.d.ts
+++ build/types/link.d.ts
@@ -35,11 +35,11 @@ declare namespace __next_route_internal_types__ {
     | `/api/printer-image`
     | `/`
     | `/calibration`
-    | `/motion`
     | `/toolhead`
     | `/wizard`
-    | `/analysis`
+    | `/motion`
     | `/analysis/macros/new`
+    | `/analysis`
     | `/analysis/macros`
   type DynamicRoutes<T extends string = string> = 
     | `/api/trpc/${SafeSlug<T>}`
