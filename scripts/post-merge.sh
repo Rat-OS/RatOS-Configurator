6aa3c1b8301fa14a0fdc7f1176e61f0f3773723b
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2024-12-19 01:01:25 +0000

    Deployed 4 commits.
    https://github.com/Rat-OS/RatOS-configurator/compare/b455b76660c8d5cbc5933fe34270cdda07c4fc46...5b88c1a448eff7dccb48d5c57f629e7c28dd4db0

--- scripts/post-merge.sh
+++ scripts/post-merge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+
+# This file is only here for backwards compatibility with the old location of the post-merge githook.
+# It will be removed in a future version.
 SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
 
-# Script dir is scripts
-sudo "$SCRIPT_DIR"/update.sh
-# Important to run this after update.sh to ensure the CLI is up to date.
-sudo "$SCRIPT_DIR"/../../configuration/scripts/ratos-update.sh
+sudo "$SCRIPT_DIR/update.sh"
