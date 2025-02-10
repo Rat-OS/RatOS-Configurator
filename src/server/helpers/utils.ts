61ca3648ed4e2fba89c137e1fb5105026d240013
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2025-02-10 15:57:42 +0100

    CLI: reload environment from tmp path when switching branches

--- src/server/helpers/utils.ts
+++ src/server/helpers/utils.ts
@@ -1,13 +1,17 @@
 import { serverSchema } from '@/env/schema.mjs';
 import { existsSync, readFileSync } from 'node:fs';
 import dotenv from 'dotenv';
+import path from 'node:path';
 
 let alreadyLoaded = false;
-export function loadEnvironment() {
+export function loadEnvironment(filePath?: string) {
 	if (alreadyLoaded) {
 		return serverSchema.parse(process.env);
 	}
-	const envFilePath = existsSync('./.env.local') ? '.env.local' : '.env';
+	const envFilePath = filePath ?? (existsSync('./.env.local') ? '.env.local' : '.env');
+	if (!existsSync(envFilePath)) {
+		throw new Error(`Environment file ${path.resolve(envFilePath)} not found`);
+	}
 	const envFile = readFileSync(envFilePath, 'utf8');
 	const env = serverSchema.parse({ NODE_ENV: 'production', ...dotenv.parse(envFile) });
 	dotenv.populate(process.env as any, env);
@@ -19,7 +23,7 @@ export function resetEnvironment() {
 	alreadyLoaded = false;
 }
 
-export function reloadEnvironment() {
+export function reloadEnvironment(filePath?: string) {
 	resetEnvironment();
-	return loadEnvironment();
+	return loadEnvironment(filePath);
 }
